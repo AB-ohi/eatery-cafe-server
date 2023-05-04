9ef26e4c805796a45455d1c4baad0650309f0b07
AB-ohi <askohi63907@gmail.com>
2023-05-04 13:55:41 +0600

    chef bio

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.port || 1000
 
 const chef = require('./data/chef.json')
 const ans = require('./data/ans.json')
+const details = require('./data/detail.json')
 
 
 app.use(cors());
@@ -19,6 +20,16 @@ app.get('/chef',(req, res) =>{
 app.get('/ans',(req, res) =>{
     res.send(ans)
 })
+app.get('/details',(req, res) =>{
+    res.send(details)
+})
+
+app.get('/details/:id'),(req,res) =>{
+    const id = req.params.id;
+    console.log(id)
+    const selectedDetail = details.find(n => n._id === id);
+    res.send(selectedDetail);
+}
 
 app.listen(port, ()=>{
     console.log(`api is running on port:${port}`)
