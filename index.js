1a21fb83c0958e8de779fedb58b93e53f8353d2a
AB-ohi <askohi63907@gmail.com>
2023-05-04 15:58:15 +0600

    add vercel

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 const  cors = require('cors')
-const port = process.env.port || 1000
+const port = process.env.PORT || 1000
+const ans = require('./data/ans.json')
 
 const chef = require('./data/chef.json')
-const ans = require('./data/ans.json')
 const details = require('./data/detail.json')
 
 
@@ -14,22 +14,19 @@ app.get('/', (req, res) =>{
     res.send('restureand is running')
 });
 
-app.get('/chef',(req, res) =>{
-    res.send(chef)
-})
 app.get('/ans',(req, res) =>{
     res.send(ans)
 })
-app.get('/details',(req, res) =>{
-    res.send(details)
-})
 
-app.get('/details/:id'),(req,res) =>{
+app.get('/chef',(req, res) =>{
+    res.send(chef)
+})
+app.get('/chef/:id',(req,res) =>{
     const id = req.params.id;
     console.log(id)
-    const selectedDetail = details.find(n => n._id === id);
+    const selectedDetail = chef.find(n => n.id === id);
     res.send(selectedDetail);
-}
+})
 
 app.listen(port, ()=>{
     console.log(`api is running on port:${port}`)
